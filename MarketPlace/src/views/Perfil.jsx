6b929eb5a1a6ec4f8476d5026f9aee8504111e81
Ariel Stancovich <arielstancovichsuarez@gmail.com>
2023-11-30 15:43:32 -0300

    update perfil
    se comienza a maquetar datos a mostrar del usuario

--- MarketPlace/src/views/Perfil.jsx
+++ MarketPlace/src/views/Perfil.jsx
@@ -1,12 +1,31 @@
 import React from 'react'
 import Nava from "../components/Nava";
+import Footer from "../components/Footer"
+import Button from 'react-bootstrap/Button';
 
 function Perfil() {
   return (
     <div>
-      <Nava/>
+      <Nava />
       <div>
-
+        <div id='datosPhone'>
+          <div className='conteinerDatos'>
+            <p><strong>Usuario:</strong> </p>
+            <p>Susana Horia de la granja</p>
+          </div>
+          <div className='conteinerDatos'>
+            <p><strong>Email:</strong> </p>
+            <p>SusanaHoria@lagranja.com</p>
+          </div>
+          <div className='conteinerDatos'>
+            <p><strong>Phone:</strong> </p>
+            <p>+56912345678</p>
+          </div>
+          <div className='conteinerDatos'>
+            <Button>Cambiar Password</Button>
+          </div>
+        </div>
+        
       </div>
     </div>
   )
