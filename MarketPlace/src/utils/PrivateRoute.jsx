555c1f30ab45af90e676567df7ba4bdbe4289588
Ariel Stancovich <arielstancovichsuarez@gmail.com>
2023-12-22 22:03:42 -0300

    update

--- MarketPlace/src/utils/PrivateRoute.jsx
+++ MarketPlace/src/utils/PrivateRoute.jsx
@@ -2,13 +2,15 @@ import { Navigate, Outlet } from "react-router-dom";
 import { useData } from "../Context/MyContext";
 import { useContext } from 'react';
 import Context from '../Context/MyContext';
+import { useNavigate } from "react-router-dom";
 
 const PrivateRoute = () => {
+  const navigate = useNavigate();
   const { user } = useData();
   const usuarioContext = useContext(Context);
   const { token, setToken } = usuarioContext;
 
-  return token ? <Outlet /> : <Navigate to="/" />;
+  return token ? <Outlet /> : navigate("/") ;
 };
 
 export default PrivateRoute;
